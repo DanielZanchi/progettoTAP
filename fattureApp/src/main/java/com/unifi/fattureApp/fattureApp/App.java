84799ac03e56720b70dd96a5a3fb555dfc10da66
Daniel Zanchi <danny.zanchi@gmail.com>
2017-04-12 11:23:01 +0200

    start

---
+++ fattureApp/src/main/java/com/unifi/fattureApp/fattureApp/App.java
@@ -0,0 +1,13 @@
+package com.unifi.fattureApp.fattureApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
