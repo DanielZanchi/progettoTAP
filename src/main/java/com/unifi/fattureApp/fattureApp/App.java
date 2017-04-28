9d1cef5f904ca3309fb9963cc056a531a95d94c7
Daniele Landi <Daniele Landi@Daniele>
2017-04-28 16:57:40 +0200

    Ti prego...funziona

--- src/main/java/com/unifi/fattureApp/fattureApp/App.java
+++ src/main/java/com/unifi/fattureApp/fattureApp/App.java
@@ -6,6 +6,6 @@ package com.unifi.fattureApp.fattureApp;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Helloooo World!!!!!");
+		System.out.println("Funziona?");
 	}
 }
