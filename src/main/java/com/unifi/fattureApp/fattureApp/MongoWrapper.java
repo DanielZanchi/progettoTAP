b41c301229abe26d04c5226469a9742b1094d493
Federico Guerri <fede95gg@gmail.com>
2017-04-28 16:05:42 +0200

    Aggiunti tests iniziali con mongoDB

--- src/main/java/com/unifi/fattureApp/fattureApp/MongoWrapper.java
+++ src/main/java/com/unifi/fattureApp/fattureApp/MongoWrapper.java
@@ -1,11 +1,31 @@
 package com.unifi.fattureApp.fattureApp;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
+import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
-public class MongoWrapper {
+public class MongoWrapper implements Database{
+
+	private MongoCollection patients;
 
 	public MongoWrapper(MongoClient myMongoClient) {
 		// TODO Auto-generated constructor stub
+		DB db=myMongoClient.getDB("medicalOffice");
+		Jongo myJongo=new Jongo(db);
+		patients=myJongo.getCollection("patients");
 	}
+
+	public List<Patients> getAllPatients() {
+		Iterable<Patients> iterable=patients.find().as(Patients.class);
+		return StreamSupport.stream(iterable.spliterator(),false).collect(Collectors.toList());
+	}
+
+	
 
 }
--- src/test/java/com/unifi/fattureApp/fattureApp/MongoWrapperTest.java
+++ src/test/java/com/unifi/fattureApp/fattureApp/MongoWrapperTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.github.fakemongo.Fongo;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
@@ -18,7 +19,7 @@ public class MongoWrapperTest {
 	@Before
 	public void init(){
 		
-		Fongo fongo=new Fongo("mongo db 1");
+		Fongo fongo=new Fongo("mongo server 1");
 		MongoClient myMongoClient=fongo.getMongo();
 		
 		DB db=myMongoClient.getDB("medicalOffice");
@@ -28,10 +29,24 @@ public class MongoWrapperTest {
 		myPatients=db.getCollection("patients");
 	}
 	
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAllPatientsEmpty(){
+		assertTrue(myMongoWrapper.getAllPatients().isEmpty());
 	}
+	
+	@Test
+	public void testNotEmptyPatients(){
+		addPatientsToDB("1", "uno");
+		assertEquals(1,myMongoWrapper.getAllPatients().size());
+	}
+	
+
+	private void addPatientsToDB(String id, String name){
+		BasicDBObject data=new BasicDBObject();
+		data.put("id",id);
+		data.put("name", name);
+		myPatients.insert(data);
+	}
+	
 
 }
