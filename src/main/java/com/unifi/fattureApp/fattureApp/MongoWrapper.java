34c70a50556596f45647c44c54c1c664fe540893
Federico Guerri <fede95gg@gmail.com>
2017-04-28 15:21:50 +0200

    Aggiunto test wrapper

--- src/main/java/com/unifi/fattureApp/fattureApp/MongoWrapper.java
+++ src/main/java/com/unifi/fattureApp/fattureApp/MongoWrapper.java
@@ -1,5 +1,11 @@
 package com.unifi.fattureApp.fattureApp;
 
+import com.mongodb.MongoClient;
+
 public class MongoWrapper {
+
+	public MongoWrapper(MongoClient myMongoClient) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/test/java/com/unifi/fattureApp/fattureApp/MongoWrapperTest.java
+++ src/test/java/com/unifi/fattureApp/fattureApp/MongoWrapperTest.java
@@ -2,11 +2,32 @@ package com.unifi.fattureApp.fattureApp;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 
 public class MongoWrapperTest {
 	
 	MongoWrapper myMongoWrapper; 
+	DBCollection myPatients;
+	
+	@Before
+	public void init(){
+		
+		Fongo fongo=new Fongo("mongo db 1");
+		MongoClient myMongoClient=fongo.getMongo();
+		
+		DB db=myMongoClient.getDB("medicalOffice");
+		db.getCollection("patients").drop();
+		
+		myMongoWrapper=new MongoWrapper(myMongoClient);
+		myPatients=db.getCollection("patients");
+	}
+	
 
 	@Test
 	public void test() {
