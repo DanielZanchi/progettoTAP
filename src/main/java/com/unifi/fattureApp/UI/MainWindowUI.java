0981de5bad45372f3867ebda431fc1ef8f8c6c4e
Federico Guerri <fede95gg@gmail.com>
2017-05-19 15:21:49 +0200

    aggiunto horizontal box per controllare la gravità dei pulsanti

--- src/main/java/com/unifi/fattureApp/UI/MainWindowUI.java
+++ src/main/java/com/unifi/fattureApp/UI/MainWindowUI.java
@@ -3,10 +3,9 @@ package com.unifi.fattureApp.UI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-
-import org.hamcrest.SelfDescribing;
 
 
 import java.awt.BorderLayout;
@@ -16,6 +15,9 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.Box;
 
 public class MainWindowUI {
 
@@ -55,32 +57,38 @@ public class MainWindowUI {
 		frame.setBounds(100, 100, 648, 426);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{636, 0};
-		gridBagLayout.rowHeights = new int[]{103, 266, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{636, 0, 0};
+		gridBagLayout.rowHeights = new int[]{103, 266, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JPanel panel = new JPanel();
+		
 		panel.setBackground(Color.PINK);
 		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 0);
-		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 0;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		JLabel label=new JLabel("ciao");
+		panel.add(label);
 		frame.getContentPane().add(panel, gbc_panel);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.RED);
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.gridwidth = 2;
-		gbc_panel_1.gridheight = 2;
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 1;
-		frame.getContentPane().add(panel_1, gbc_panel_1);
+		Box verticalBox = Box.createVerticalBox();
+		GridBagConstraints gbc_verticalBox = new GridBagConstraints();
+		gbc_verticalBox.anchor = GridBagConstraints.SOUTH;
+		gbc_verticalBox.gridheight = 3;
+		gbc_verticalBox.gridx = 0;
+		gbc_verticalBox.gridy = 1;
+		frame.getContentPane().add(verticalBox, gbc_verticalBox);
+		
+		JButton btnNewButton = new JButton("New button");
+		verticalBox.add(btnNewButton);
+		
+		
+		
 		
 		
 	}
-
 }
