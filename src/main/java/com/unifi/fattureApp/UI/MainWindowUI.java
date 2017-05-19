6c16e163f6ce2bd733c4ec7c91e8a4314a9cdec6
Federico Guerri <fede95gg@gmail.com>
2017-05-19 14:53:34 +0200

    i pannelli si ridimensionano automaticamente

--- src/main/java/com/unifi/fattureApp/UI/MainWindowUI.java
+++ src/main/java/com/unifi/fattureApp/UI/MainWindowUI.java
@@ -8,14 +8,22 @@ import javax.swing.WindowConstants;
 
 import org.hamcrest.SelfDescribing;
 
-import com.vividsolutions.jts.geom.Dimension;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 public class MainWindowUI {
 
 	private JFrame frame;
+	private static final int DEFAULT_WINDOW_DIMENS = 500;
+
+	private int windowDefaultWidth=DEFAULT_WINDOW_DIMENS;
+	private int windowDefaultHeight=DEFAULT_WINDOW_DIMENS;
 
 	/**
 	 * Launch the application.
@@ -42,12 +50,37 @@ public class MainWindowUI {
 
 	private void initialize() {
 		frame = new JFrame();
-		frame.setMinimumSize(new Dimension(100, 100));
+		frame.setMinimumSize(new Dimension(DEFAULT_WINDOW_DIMENS,DEFAULT_WINDOW_DIMENS));
+		
+		frame.setBounds(100, 100, 648, 426);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{636, 0};
+		gridBagLayout.rowHeights = new int[]{103, 266, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel);
+		panel.setBackground(Color.PINK);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		frame.getContentPane().add(panel, gbc_panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.RED);
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridwidth = 2;
+		gbc_panel_1.gridheight = 2;
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		frame.getContentPane().add(panel_1, gbc_panel_1);
+		
+		
 	}
 
 }
