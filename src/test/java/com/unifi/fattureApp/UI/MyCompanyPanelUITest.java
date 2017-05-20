9936421e16bab47e75309b1c5ceec908d1735542
Daniel Zanchi <danny.zanchi@gmail.com>
2017-05-20 15:47:25 +0200

    test companypanel

--- src/test/java/com/unifi/fattureApp/UI/MyCompanyPanelUITest.java
+++ src/test/java/com/unifi/fattureApp/UI/MyCompanyPanelUITest.java
@@ -7,29 +7,33 @@ package com.unifi.fattureApp.UI;
 
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JPanelFixture;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MyCompanyPanelUITest {
-	
-private FrameFixture window;
-	
-	@Before
-	public void setUp() {
+
+	private FrameFixture window;
+
+	private JPanelFixture myCompany_Panel;
+
+	@Before public void setUp() {
 		MainWindowUI frame = GuiActionRunner.execute(() -> new MainWindowUI());
 		window = new FrameFixture(frame.getMainFrame());
+//		myCompany_Panel = window.panel("CompanyPanel");
 		window.show();
 	}
-	
+
 	@After
 	public void tearDown() {
-	  window.cleanUp();
+		window.cleanUp();
 	}
-	
+
 	@Test
-	public void testAddCompanyButton() {
-		window.panel("CompanyPanel");
+	public void testCompanyPanelBackground() {
+//		myCompany_Panel.background().requireEqualTo(new java.awt.Color(232,230,230));
+		window.panel("CompanyPanel").background().requireEqualTo(new java.awt.Color(232,230,230));
 	}
-	
+
 }
