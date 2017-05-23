74dfb8c5ebd9d8455d744fd2f499b2d217d69136
Federico Guerri <fede95gg@gmail.com>
2017-05-23 14:02:13 +0200

    Aggiunta classe Invoice

---
+++ src/main/java/com/unifi/fattureApp/App/Invoice.java
@@ -0,0 +1,58 @@
+package com.unifi.fattureApp.App;
+
+public class Invoice {
+
+	
+	
+	private String id;
+	private String name;
+	private String price;
+	private String description;
+
+	public Invoice() {
+
+	}
+
+	public Invoice(String id, String name,String price,String description) {
+		this.setId(id);
+		this.setName(name);
+		this.setPrice(price);
+		this.setDescription(description);
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
