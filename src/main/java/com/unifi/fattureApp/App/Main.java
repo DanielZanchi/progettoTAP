72472941a7109c8f8542b3ebb63ee8fc84893b01
Federico Guerri <fede95gg@gmail.com>
2017-12-04 18:19:31 +0100

    prova gui travis

--- src/main/java/com/unifi/fattureApp/App/Main.java
+++ src/main/java/com/unifi/fattureApp/App/Main.java
@@ -2,13 +2,15 @@ package com.unifi.fattureApp.App;
 
 import java.net.UnknownHostException;
 
+import com.unifi.fattureApp.UI.MainWindowUI;
+
 public class Main {
 	public static void main(String[] args) throws UnknownHostException {
 		//Launch UI
 		//		new MainWindowUI(new MongoUiComunication());
 		MongoUiComunication mongoUiComunication = new MongoUiComunication(false, args);
 
-		//new MainWindowUI(mongoUiComunication);
+		new MainWindowUI(mongoUiComunication);
 
 		//		Fill db
 		/*
