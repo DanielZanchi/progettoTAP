66da2bf77d06244b11027c881d180b2e0ece8bf5
Federico Guerri <fede95gg@gmail.com>
2017-12-05 16:52:38 +0100

    Modificato dockerfile, aggiunto controllo nel main per docker-container.

--- src/main/java/com/unifi/fattureApp/App/Main.java
+++ src/main/java/com/unifi/fattureApp/App/Main.java
@@ -6,31 +6,17 @@ import com.unifi.fattureApp.UI.MainWindowUI;
 
 public class Main {
 	public static void main(String[] args) throws UnknownHostException {
-		//Launch UI
-		//		new MainWindowUI(new MongoUiComunication());
+		
+		
 		MongoUiComunication mongoUiComunication = new MongoUiComunication(false, args);
 
-		new MainWindowUI(mongoUiComunication);
-
-		//		Fill db
-		/*
-		int companies=2;
-		int clients=4;
-		int invoices=6;
-		for(int i=0;i<companies;i++) {
-			mongoUiComunication.addCompanyToDatabase("Compagnia "+String.valueOf(i), String.valueOf(i), "via indirizzo "+String.valueOf(i), "citta' "+String.valueOf(i), "AR", "zipcompany "+String.valueOf(i), "IT", "", "email "+String.valueOf(i));
-			System.out.println("aggiunte "+i+" companies ");
+		//Launch UI
+		MainWindowUI mainWindowUI=null;
+		try {
+			mainWindowUI=new MainWindowUI(mongoUiComunication);
+		}catch (Exception e) {
+			System.out.println("In docker container, gui not running");
 		}
-		for(int i=0;i<clients;i++) {
-			mongoUiComunication.addClientToDatabase("Nome Cognome"+String.valueOf(i),String.valueOf(i), "indirizzo cliente "+String.valueOf(i), "citta' cliente "+String.valueOf(i), "Ar", "zipcliente "+String.valueOf(i), "IT", "", "");
-			System.out.println("aggiunti "+i+" clients ");
-		}
-		for(int i=0;i<invoices;i++) {
-			mongoUiComunication.addInvoiceToDatabase("Nome Fattura"+String.valueOf(i), String.valueOf(i*6), "descrizione fattura "+String.valueOf(i));
-			System.out.println("aggiunte "+i+" invoices ");
-		}
-		 */
-		//		endFill db
 
 		System.out.println("Fatture-app terminates");
 	}
