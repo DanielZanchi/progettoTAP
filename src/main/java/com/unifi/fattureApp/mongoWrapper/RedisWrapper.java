752fa60e82de1af05b817d9837fc8212d4d7aa13
Federico Guerri <fede95gg@gmail.com>
2018-01-05 00:54:56 +0100

    Aggiunto redis

---
+++ src/main/java/com/unifi/fattureApp/mongoWrapper/RedisWrapper.java
@@ -0,0 +1,71 @@
+package com.unifi.fattureApp.mongoWrapper;
+
+import java.util.List;
+
+import com.lambdaworks.redis.RedisClient;
+import com.unifi.fattureApp.App.Client;
+import com.unifi.fattureApp.App.Company;
+import com.unifi.fattureApp.App.Database;
+import com.unifi.fattureApp.App.Invoice;
+
+public class RedisWrapper implements Database{
+	
+	public  RedisWrapper(RedisClient rClient) {
+
+	}
+
+	@Override
+	public List<Client> getAllClientsList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client findClientById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Company> getAllCompaniesList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Company findCompanyById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveCompany(Company company) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Invoice> getAllInvoicesList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Invoice findInvoiceById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveInvoice(Invoice invoice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
