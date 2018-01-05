60909cfc2f20dbc50529f96b0ba433c2f33e259d
Daniele Landi <dani.landi.dl@gmail.com>
2018-01-06 00:43:40 +0100

    mah

--- src/main/java/com/unifi/fattureApp/App/Main.java
+++ src/main/java/com/unifi/fattureApp/App/Main.java
@@ -2,12 +2,9 @@ package com.unifi.fattureApp.App;
 
 import java.net.UnknownHostException;
 
-import org.apache.log4j.Logger;
-
 import com.unifi.fattureApp.UI.MainWindowUI;
 
 public class Main {
-	private static final Logger LOGGER = Logger.getLogger(Main.class);
 	public static void main(String[] args) throws UnknownHostException {
 		MongoUiComunication mongoUiComunication = new MongoUiComunication(false, args);
 
@@ -18,8 +15,6 @@ public class Main {
 		}catch (Exception e) {
 			System.out.println("In docker container, gui not running");
 		}		
-//		System.out.println("Fatture-app terminates");
-		String s = "Fatture-app terminates";
-		LOGGER.debug(s);
+		System.out.println("Fatture-app terminates");
 	}
 }
