bafa5d0e6446b8dc5b5f033f7fe6a1d5de3b0b3b
Daniele Landi <dani.landi.dl@gmail.com>
2018-02-16 15:40:27 +0100

    prova redis in docker-compose

--- src/main/java/com/unifi/fattureApp/App/Invoice.java
+++ src/main/java/com/unifi/fattureApp/App/Invoice.java
@@ -18,19 +18,19 @@ public class Invoice implements Serializable {
 		this.setPrice(price);
 		this.setDescription(description);
 	}
-
+	
 	public String getId() {
 		return id;
 	}
-
+	
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -50,109 +50,60 @@ public class Invoice implements Serializable {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
-	
+
 	@Override
 	public int hashCode(){
-	
-	            final int prime = 31;
-	
-	            int result = 1;
-	
-	            result = prime * result + ((price == null) ? 0 : price.hashCode());
-	
-	            result = prime * result + ((description == null) ? 0 : description.hashCode());
-	
-	            result = prime * result + ((id == null) ? 0 : id.hashCode());
-	
-	            result = prime * result + ((name == null) ? 0 : name.hashCode());
-	
-	            return result;
-	
-	    }
-	
-	 
-	
-	    @Override
-	
-	    public boolean equals(Object obj)
-	
-	    {
-	
-	            if (this == obj)
-	
-	                    return true;
-	
-	            if (obj == null)
-	
-	                    return false;
-	
-	            if (getClass() != obj.getClass())
-	
-	                    return false;
-	
-	            Invoice other = (Invoice) obj;
-	
-	            if (price != other.price)
-	
-	                    return false;
-	
-	            if (description == null)
-	
-	            {
-	
-	                    if (other.description != null)
-	
-	                            return false;
-	
-	            }
-	
-	            else if (!description.equals(other.description))
-	
-	                    return false;
-	
-	            if (id == null)
-	
-	            {
-	
-	                    if (other.id != null)
-	
-	                            return false;
-	
-	            }
-	
-	            else if (!id.equals(other.id))
-	
-	                    return false;
-	
-	            if (name == null)
-	
-	            {
-	
-	                    if (other.name != null)
-	
-	                            return false;
-	
-	            }
-	
-	            else if (!name.equals(other.name))
-	
-	                    return false;
-	
-	            return true;
-	
-	    }
-	
-	 
-	
-	    @Override
-	
-	    public String toString(){
-	            return "Invoice [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
-	    }  
+		final int prime = 31;
+		int result = 1;
 
-	
-	
-	
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (getClass() != obj.getClass())
+			return false;
+
+		Invoice other = (Invoice) obj;
+		if (price != other.price)
+			return false;
+
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		}
+		else if (!description.equals(other.description))
+			return false;
+
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		}
+		else if (!id.equals(other.id))
+			return false;
+
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString(){
+		return "Invoice [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+	}  
 }
