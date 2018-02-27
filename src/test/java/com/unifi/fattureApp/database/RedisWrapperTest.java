f58c0790fb10db2a88b092af675b32c45dbb429d
Federico Guerri <fede95gg@gmail.com>
2018-02-27 18:51:29 +0100

    refactoring test dei wrapper

--- src/test/java/com/unifi/fattureApp/database/RedisWrapperTest.java
+++ src/test/java/com/unifi/fattureApp/database/RedisWrapperTest.java
@@ -3,16 +3,19 @@ package com.unifi.fattureApp.database;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import org.junit.After;
 import org.slf4j.LoggerFactory;
+
+import com.unifi.fattureApp.helpTestTools.TestHelperTool;
 
 import ch.qos.logback.classic.LoggerContext;
 import redis.embedded.RedisServer;
 
-public class RedisWrapperTests extends RedisWrapperTestAbstract{
+public class RedisWrapperTest extends AbstractWrapperTest{
 	private RedisServer redisServer;
 
 	@Override
-	public void createRedis() throws UnknownHostException {
+	public void init() throws UnknownHostException {
 		try {
 			redisServer = new RedisServer(6379);
 			redisServer.start();
@@ -21,9 +24,11 @@ public class RedisWrapperTests extends RedisWrapperTestAbstract{
 		}
 		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		loggerContext.stop();
+		testHelper = new TestHelperTool();
+		database = testHelper.usingRedis();
 	}
 
-	@Override
+	@After
 	public void stopRedis() {
 		redisServer.stop();
 	}
