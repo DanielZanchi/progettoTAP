8e68831175b904432b56dea224151fdae7840607
dani1993 <dani.landi.dl@gmail.com>
2018-05-11 14:18:59 +0200

    modificato main per lanciare app con redis

--- src/main/java/com/unifi/fattureApp/App/Main.java
+++ src/main/java/com/unifi/fattureApp/App/Main.java
@@ -10,9 +10,9 @@ public class Main {
 	private static final Logger LOGGER = Logger.getLogger(Main.class);
 	public static void main(String[] args) throws IOException {
 		DatabaseUiComunication mongoUiComunication;
-		if(args.length>1) {//redis
+		if(args.length==1 && args[0]=="redis") {//redis
 			mongoUiComunication = new RealDatabaseUiComunication(args, false);
-		}else {//mongo
+		} else {//mongo
 			mongoUiComunication = new RealDatabaseUiComunication(args, true);
 		}
 
